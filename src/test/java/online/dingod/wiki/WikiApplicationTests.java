919bdb797f117b267df8f4d0ac38a92ad7418918
DingAllen <1121174884@qq.com>
2022-01-31 10:47:44 +0800

    2-2 init

---
+++ src/test/java/online/dingod/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package online.dingod.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
