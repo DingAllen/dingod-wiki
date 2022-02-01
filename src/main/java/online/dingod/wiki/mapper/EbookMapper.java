b0a9d0608215fc0bdd2fcf6ea831219c2d2a9f56
DingAllen <1121174884@qq.com>
2022-02-01 09:54:57 +0800

    3-6 新增ebook表，生成持久层代码

---
+++ src/main/java/online/dingod/wiki/mapper/EbookMapper.java
@@ -0,0 +1,30 @@
+package online.dingod.wiki.mapper;
+
+import java.util.List;
+import online.dingod.wiki.domain.Ebook;
+import online.dingod.wiki.domain.EbookExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+}
