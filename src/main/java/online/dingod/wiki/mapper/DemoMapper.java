e28b139a67d7ca5dbcfcf60746774ec149f46985
DingAllen <1121174884@qq.com>
2022-02-01 09:02:21 +0800

    3-5 集成mybatis generator,自动生成以Demo为命名的一系列代码文件

---
+++ src/main/java/online/dingod/wiki/mapper/DemoMapper.java
@@ -0,0 +1,30 @@
+package online.dingod.wiki.mapper;
+
+import java.util.List;
+import online.dingod.wiki.domain.Demo;
+import online.dingod.wiki.domain.DemoExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    Demo selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByPrimaryKeySelective(Demo record);
+
+    int updateByPrimaryKey(Demo record);
+}
