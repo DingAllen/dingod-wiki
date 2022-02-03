efa568f4d65f9dc96b855e031c85b83aa73e45d8
DingAllen <1121174884@qq.com>
2022-02-03 10:50:12 +0800

    5-4 全局集成图标组件 修复数据库问题（该问题我也不知道怎么回事，总之能跑起来不报错就问题不大了）

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -46,14 +46,44 @@
       </a-menu>
     </a-layout-sider>
     <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-      <pre>{{ ebooks }}</pre>
-      <pre>{{ ebooks2 }}</pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
+/*import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
 import axios from "axios";
 
 export default defineComponent({
@@ -76,5 +106,42 @@ export default defineComponent({
       ebooks2: toRef(ebooks1, "books")
     }
   }
+});*/
+import { defineComponent } from 'vue';
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions: Record<string, string>[] = [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ];
+    return {
+      listData,
+      pagination,
+      actions,
+    };
+  },
 });
 </script>
