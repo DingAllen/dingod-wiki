16f0411f324e632ce25297f3a2367c67550bc178
DingAllen <1121174884@qq.com>
2022-02-06 15:14:44 +0800

    5-9 增加AOP，打印接口耗时，请求参数和返回参数 去掉过滤器、拦截器

--- src/main/java/online/dingod/wiki/config/SpringMvcConfig.java
+++ src/main/java/online/dingod/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,21 @@
-package online.dingod.wiki.config;
-
-import online.dingod.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**");
-    }
-}
+//package online.dingod.wiki.config;
+//
+//import online.dingod.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**");
+//    }
+//}
